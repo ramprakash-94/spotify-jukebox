8871ce77ebb050001499b6138a3283a69c414eb5
Ram Prakash <arivuchelvan.r@husky.neu.edu>
2019-06-27 17:58:10 -0400

    Updated prod server url

--- src/config.js
+++ src/config.js
@@ -6,6 +6,6 @@ if (mode === "dev"){
     url = "http://localhost:4000"
 }
 else if (mode === "prod"){
-    url = "http://ec2-54-196-77-140.compute-1.amazonaws.com:4000"
+    url = "https://jukebox-graphql.herokuapp.com"
 }
 export default url
